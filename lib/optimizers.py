8a35c5bf1e20e9fbb53c341dbf682eaf7f87a822
Akshay <59740725+Killua7362@users.noreply.github.com>
2023-10-17 10:01:54 +0300

    update

--- lib/optimizers.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class SGD:
-    def __init__(self,lr=0.01):
-        self.learning_rate  = lr
-
-    def update_params(self,layer):
-        for k,v in layer.trainable_params.items():
-            layer.trainable_params[k] =  layer.trainable_params[k] + (-self.learning_rate * getattr(layer,f'd{k}'))
