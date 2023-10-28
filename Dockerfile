00bb5d04faabed5355c646f4500af5f0a9708a68
Akshay <59740725+Killua7362@users.noreply.github.com>
2023-10-28 19:03:55 +0300

    revert

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,8 @@
-# Use the official lightweight Python image.
-# https://hub.docker.com/_/python
-FROM python:3.7-slim
-
-# Copy local code to the container image.
-ENV APP_HOME /app
-WORKDIR $APP_HOME
+FROM python:3.9-slim-buster
+WORKDIR /app
+COPY ./requirements.txt /app
+RUN pip install -r requirements.txt
 COPY server.py /app
-
-# Install production dependencies.
-RUN pip install Flask gunicorn
-
-# Run the web service on container startup. Here we use the gunicorn
-# webserver, with one worker process and 8 threads.
-# For environments with multiple CPU cores, increase the number of workers
-# to be equal to the cores available.
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 server:server
+EXPOSE 8080
+ENV FLASK_APP=server.py
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-import os
 
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
 from flask import Flask
-
+ 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
 app = Flask(__name__)
-
+ 
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
 @app.route('/')
+# ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
-
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    return 'Hello World'
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host='0.0.0.0',port=int(5000))
